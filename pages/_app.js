1906d3e0b798b0e3c49fef7ed1ad58dd2a2a6cea
AdonaiA <aitgabell@gmail.com>
2022-07-12 23:32:12 +0200

    Add UI for Home, Navbar & Footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { Navbar, Footer } from '../components';
 
-export default MyApp
+import '../styles/globals.css';
+
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <div className="pt-65">
+        <Component {...pageProps} />
+      </div>
+      <Footer />
+    </div>
+    <Script src="https://kit.fontawesome.com/82d262d3f9.js" crossorigin="anonymous" />
+  </ThemeProvider>
+);
+
+export default MyApp;
